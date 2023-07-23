166c4db2d0350a85241e544f6616b6cc1f2917ec
Elioenay <elionay407@gmail.com>
2023-07-22 21:25:12 -0300

    Estrutura da aplicação - Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from 'styled-components'
 import Button from './components/Button'
+import { defaultTheme } from './styles/default'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
