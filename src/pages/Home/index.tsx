4c5dcdd1da9e8ee8a70f2556c5b8c214f4c2bc9a
Elioenay <elionay407@gmail.com>
2023-07-23 10:52:05 -0300

    Página: Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <form action=''>
+        <FormContainer>
+          <label htmlFor='task'>Vou Trabalhar em</label>
+          <input
+            type='text'
+            id='task'
+          />
+
+          <label htmlFor='minutesAmount'>Durante</label>
+          <input
+            type='number'
+            id='minutesAmount'
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type='submit'>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
