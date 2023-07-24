cc5aea9b3bf86f00c742034df34efa15f25e34ae
Elioenay <elionay407@gmail.com>
2023-07-23 23:11:19 -0300

    :bug: Fix import from history page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import DefaultLayout from './layouts/DefaultLayout'
 import History from './pages/History'
+import DefaultLayout from './layouts/DefaultLayout'
 
 export default function Router() {
   return (
